9e9c983d12fbb200d701bd8f69221b065176c0b7
randylkays <randylkays@gmail.com>
2024-02-04 12:58:48 -0800

    Update mpSRU0.bat
    Change the new Surface PICO (PICO3) script

--- mpSRU0.bat
+++ mpSRU0.bat
@@ -1,5 +1,5 @@
 d:
-cd D:\PythonPrograms\Pico3\GitHub\Serial_Read_Usart0
+cd D:\PythonPrograms\Pico3\GitHub\FishSerialCmdAndRead
 mpremote rtc --set
 timeout 5
-mpremote mount D:\PythonPrograms\Fish_support\Github run Serial_Read_Usart0.py
+mpremote mount D:\PythonPrograms\Fish_support\Github run FishSerialCmdAndRead.py
