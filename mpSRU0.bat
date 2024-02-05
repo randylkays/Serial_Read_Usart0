7ae2abeec5c331b292e5dd6ab0939c2519b4363a
randylkays <randylkays@gmail.com>
2024-02-04 16:15:29 -0800

    Update mpSRU0.bat
    Unbroke directory

--- mpSRU0.bat
+++ mpSRU0.bat
@@ -1,5 +1,5 @@
 d:
-cd D:\PythonPrograms\Pico3\GitHub\FishSerialCmdAndRead
+cd D:\PythonPrograms\Pico3\GitHub\Serial_Read_Usart0
 mpremote rtc --set
 timeout 5
 mpremote mount D:\PythonPrograms\Fish_support\Github run FishSerialCmdAndRead.py
