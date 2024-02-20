45e675f977b1b85343dbfffcb6a3ee1a84fe7f07
randylkays <randylkays@gmail.com>
2024-02-20 10:44:33 -0800

    Update ButtonTest.py
    End with exit, instead of exception

--- ButtonTest.py
+++ ButtonTest.py
@@ -1,14 +1,20 @@
 from machine import Pin
 import time
+from sys import exit
               
 button = Pin(13, Pin.IN, Pin.PULL_UP)    #Create button object from Pin13 , Set GP13 to input
 led = Pin(25, Pin.OUT)   # create LED object from Pin 25, Set Pin 15 to output
-
+print("Push the button")
 try:
     while True:
         if not button.value():
-            led.value(1)                #Set led turn on
+            led.value(0)
+            print("Button Pushed - Bye")
+            exit()
         else:
-            led.value(0)                #Set led turn off
+            led.value(1)                #Set led turn off
+
+
 except:
     pass
+
